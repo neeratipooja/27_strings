56c820dd4824a5f5a2dbd3571d7c89dab9ac74f2
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-16 18:25:38 +0530

    Add files via upload

---
+++ strings1.py
@@ -0,0 +1,12 @@
+'''
+DATE=16TH NOV 2022
+DAY= WEDNESDAY
+TOPIC= STRINGS
+AUTHOR= POOJA
+'''
+k='global'
+print(k.index('b')) #3
+print(k.index('g',0)) #0
+print(k.index('a')) #4
+print(k.index('b',0)) #3
+print(len(k)) #6
